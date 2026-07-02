30adfb204addd50815927b2b34e67c6c1f3f3afb
RaphBen <raph_ben@hotmail.com>
2026-07-02 22:49:54 +0200

    [CHANGED] Updated siteweb

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.ludiven-studio.fr',
-    integrations: [react(), sitemap()],
+    integrations: [react(), sitemap({ filter: (page) => !page.includes('/labo') })],
 });
